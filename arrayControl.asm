241d8745c94e022c3590c3822d879bd9d69926b7
KTrinity <35970207+KTrinity@users.noreply.github.com>
2019-02-12 15:48:04 -0800

    Add files via upload

---
+++ arrayControl.asm
@@ -0,0 +1,15 @@
+;Author Name: Lewis Lam
+;Program Name: Arrays
+;Names of files in this program: arrayDriver.cpp, arrayControl.asm, computeMean.asm, display.c, square.cpp, arrayRun.sh
+;Course Number: CPSC 240
+;Assignment Number: 3
+;Scheduled delivery date: March 7, 2019
+;Status: In Progress
+;Date of last modification: Feb 12, 2019
+
+;Information about this module:
+;This module's purpose: Control module for Arrays
+;File name of this module: arrayControl.asm
+;Compile this module: nasm -f elf64 -l array.lis -o arrayControl.o arrayControl.asm
+;Link this module with other objects:
+;Execute: ./array.out
---
+++ square.cpp
@@ -0,0 +1,17 @@
+/*
+Author Name: Lewis Lam
+Program Name: Arrays
+Names of files in this program: arrayDriver.cpp, arrayControl.asm, computeMean.asm, display.c, square.cpp, arrayRun.sh
+Course Number: CPSC 240
+Assignment Number: 3
+Scheduled delivery date: March 7, 2019
+Status: In Progress
+Date of last modification: Feb 12, 2019
+
+Information about this module:
+This module's purpose: Module for squaring values for Arrays
+File name of thsi module: square.cpp
+Compile this module: g++ -c -m64 -std=c++98 -o square.o square.cpp
+Link this module with other projects:
+Execute: ./array.out
+*/
