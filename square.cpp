ef3533baf9ac08e72fc4a1effb4cc82bd4fef541
KTrinity <35970207+KTrinity@users.noreply.github.com>
2019-03-28 15:33:55 -0700

    Add files via upload

--- square.cpp
+++ square.cpp
@@ -1,19 +1,7 @@
 /*
 Author Name: Lewis Lam
-Program Name: Arrays
-Names of files in this program: arrayDriver.cpp, arrayControl.asm, computeMean.asm, display.c, square.cpp, arrayRun.sh
-Course Number: CPSC 240
-Assignment Number: 3
-Scheduled delivery date: March 7, 2019
-Status: In Progress
-Date of last modification: Feb 28, 2019
-
-Information about this module:
-This module's purpose: Module for squaring values for Arrays
-File name of thsi module: square.cpp
-Compile this module: g++ -c -m64 -std=c++98 -o square.o square.cpp -fno-pie -no-pie
-Link this module with other projects: g++ -m64 -std=c++98 -o array.out arrayControl.o arrayDriver.o display.o square.o -fno-pie -no-pie
-Execute: ./array.out
+Program Name: Even Only
+March 28, 2019
 */
 
 #include <iostream>
@@ -21,7 +9,7 @@ extern "C" void display(long arr[], long size);
 
 extern "C" void square(long arr[], long size) {
   for(int i = 0; i < size; ++i) {
-    arr[i] *= arr[i];
+    arr[i] *= arr[i]; //Squares each element in the array (Doesn't change original array)
   }
-  display(arr, size);
+  display(arr, size); //Calls display function with new array
 }
